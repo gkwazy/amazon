a01f43eb8a857abe0bf155eb6e8908e3200846ca
gkwazy <noreply>
2018-07-14 00:12:03 -0600

    completed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,33 +18,42 @@ function start() {
     inquirer.prompt({
         name: "itemWanted",
         type: "input",
-        message: " What item would you like to buy? (enter Item ID number):\n"
+        message: "\n What item would you like to buy? (enter Item ID number):\n"
     }).then(function (response) {
+
         connection.query("Select * from products", function (err, results) {
-            for (i = 0; i < results.lenght; i++) {
-                if (results[i] === itemWanted && results[i].stock_quantitly > 0) {
-                    var newStock = results[i].stock_quantitly - 1;
-                    connection.query("update products set ? where ?", [
-                        {
-                            stock_quantitly: newStock
-                        },
-                        {
-                            item_id: results[i].item_id
-                        }
-                    ],
-                        function (error) {
-                            if (error) throw error;
-                            console.log("You have purchased your item, we will " +
-                                "deliver it in your dreams");
-                            itemList();
-                        }
-                    )
-                } else {
-                    console.log("we sold out of that item, aka your too slow.")
-                    itemList();
+            var itemfound = false;
+            for (i = 0; i < results.length; i++) {
+                if (results[i].item_id == response.itemWanted) {
+                    itemfound = true;
+                    if (results[i].stock_quantity > 0) {
+                        var newStock = results[i].stock_quantity - 1;
+                        connection.query("update products set ? where ?", [
+                            {
+                                stock_quantity: newStock
+                            },
+                            {
+                                item_id: results[i].item_id
+                            }
+                        ],
+                            function (error) {
+                                if (error) throw error;
+                                console.log("You have purchased your item, we will " +
+                                    "deliver it in your dreams");
+
+                            }
+                        )
+                    } else {
+                        console.log("Sorry we are out of the item, I will work on restocking it.")
+                    }
                 }
             }
+            if (itemfound === false) {
+                console.log("Sorry we could not find a item with that id. ")
+            }
+            itemList();
         });
+
 
     });
 };
@@ -53,16 +62,9 @@ function itemList() {
     connection.query("SELECT * FROM products", function (err, results) {
         console.log("Here are the items we have for sell:")
         for (i = 0; i < results.length; i++) {
-            console.log(results[i].product_name + "\n");
-            console.log(`id: ${results[i].item_id} | name: ${results[i].product_name}  `);
-
+            console.log(` ${results[i].product_name}-id: ${results[i].item_id}   `);
         }
         start();
     });
 
 }
-// console.log(somefunc)
-// function somefunc(params) {
-
-// }
-// console.log(somefunc)
