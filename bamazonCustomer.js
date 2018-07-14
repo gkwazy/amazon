1991569b55f89d5fd29c350a7756c3e8a4a121af
gkwazy <no-replay>
2018-07-13 23:12:54 -0600

    after help

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,29 +1,68 @@
-var mysql = requier("mysql");
-var inquirer = requier("inquirer");
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "root",
-    database: "products"
+    database: "bamazon"
 });
 
 connection.connect(function (err) {
     console.log("Connected")
-    start();
+    itemList();
 });
 
-var start = function () {
-    itemList();
+function start() {
+
     inquirer.prompt({
         name: "itemWanted",
         type: "input",
         message: " What item would you like to buy? (enter Item ID number):\n"
     }).then(function (response) {
+        connection.query("Select * from products", function (err, results) {
+            for (i = 0; i < results.lenght; i++) {
+                if (results[i] === itemWanted && results[i].stock_quantitly > 0) {
+                    var newStock = results[i].stock_quantitly - 1;
+                    connection.query("update products set ? where ?", [
+                        {
+                            stock_quantitly: newStock
+                        },
+                        {
+                            item_id: results[i].item_id
+                        }
+                    ],
+                        function (error) {
+                            if (error) throw error;
+                            console.log("You have purchased your item, we will " +
+                                "deliver it in your dreams");
+                            itemList();
+                        }
+                    )
+                } else {
+                    console.log("we sold out of that item, aka your too slow.")
+                    itemList();
+                }
+            }
+        });
 
     });
 };
 
-var itemList = function () {
+function itemList() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        console.log("Here are the items we have for sell:")
+        for (i = 0; i < results.length; i++) {
+            console.log(results[i].product_name + "\n");
+            console.log(`id: ${results[i].item_id} | name: ${results[i].product_name}  `);
+
+        }
+        start();
+    });
 
 }
+// console.log(somefunc)
+// function somefunc(params) {
+
+// }
+// console.log(somefunc)
--- sqlBamazon.sql
+++ sqlBamazon.sql
@@ -1,10 +1,17 @@
 create database bamazon;
 use bamazon;
 create table products
-    item_id integer not null auto_increment,
-    product_name varchar(50) not null,
-    depertment_name varchar(50) not null,
-    price decimal(13,2),
-    stock_quantity integer(50) default (0),
-    primary key (item_id)
+(
+    item_id integer not null
+    auto_increment,
+    product_name varchar
+    (50) not null,
+    depertment_name varchar
+    (50) not null,
+    price decimal
+    (13,2),
+    stock_quantity integer
+    (50) default 0,
+    primary key
+    (item_id)
     );
